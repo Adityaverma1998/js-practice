3fa9f889c4a93cc2551422325d2d587a3dcd3863
aditya <aditya@bucle.dev>
2024-12-11 22:06:47 +0530

    add: add basic pattern question

--- closures/script.js
+++ closures/script.js
@@ -8,29 +8,29 @@
 
 // basic function to the closure
 
-function outer() {
-    let x = 1;
-    function inner() {
-      let y = 2;
-      console.log(x + " " + y);
-    }
-    return inner;
-  }
+// function outer() {
+//     let x = 1;
+//     function inner() {
+//       let y = 2;
+//       console.log(x + " " + y);
+//     }
+//     return inner;
+//   }
   
-  let closureFunction = outer();
-  closureFunction();
+//   let closureFunction = outer();
+//   closureFunction();
 
 
 
-  let f = (a) => {
-    let i = 1;
-    let j = 2; //  j will not be included in closure as long as it is not used inside the inner function
-    return ()=> {
-        console.log(i +"a");
-    }
-}
+//   let f = (a) => {
+//     let i = 1;
+//     let j = 2; //  j will not be included in closure as long as it is not used inside the inner function
+//     return ()=> {
+//         console.log(i +"a");
+//     }
+// }
 
-console.dir(f(1)());
+// console.dir(f(1)());
 
 
 
@@ -52,17 +52,17 @@ console.dir(f(1)());
 
 // //   The local variables are not copied — they are kept by reference. In other words, the value of the variables inside a closure can be modified.
   
-// let x = 2;
-// function outer() {                   
-//   return function inner(y) {  
-//     return x * y               
-//   }                    
-// }
-// let myClosure = outer()
-// myClosure(5) // 10
-// x = 4 // value of x will be updated to 4;
+let x = 2;
+function outer() {                   
+  return function inner(y) {  
+    return x * y               
+  }                    
+}
+let myClosure = outer()
+console.log(myClosure(5))
+x = 4 
 
-// myClosure(5) // 20
+console.log(myClosure(5))
 
 // // We can use closures to store private data.
 // function initSalary() {
