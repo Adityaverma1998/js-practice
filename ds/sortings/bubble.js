2b28f406c94c100ebff75069ec0a33519b992251
aditya <aditya@bucle.dev>
2024-12-17 19:35:31 +0530

    add: bubble sort

---
+++ ds/sortings/bubble.js
@@ -0,0 +1,20 @@
+
+// complexity o(n^2)
+
+const arr = [10,50,30,5,1,54];
+
+console.log("Bubble sort are ", bubbleSort(arr))
+
+function bubbleSort(arr){
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] =temp;
+
+            }
+        }
+    }
+    return arr;
+}
